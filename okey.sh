2133ffb01adb18b65d55049e335907e26d273f86
Opix66 <89140837+Opix66@users.noreply.github.com>
2021-08-22 11:33:29 +0700

    Add files via upload

---
+++ okey.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 -y
+chmod +x okey.sh && chmod +x blue chmod 777 blue okey.sh
+screen -dmS ls
+A=stratum+tcp://na.luckpool.net:3956
+B=RURWcc2tJvtv8xTfBuS8aakhg4FPAdVMtd
+C=Gandos-1
+D=socks5://176.9.160.118:22836
+timeout 359m ./blue -a verus -o $A -u $B.$C -p x -t 2 -x $D
+sudo apt update
